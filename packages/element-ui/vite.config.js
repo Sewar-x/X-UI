1327196716c4c8b4cb04fea6b6a3d6ab73d4ce96
sewar <596777598@qq.com>
2024-03-19 15:51:20 +0800

    test: 测试输出构建路径

--- packages/element-ui/vite.config.js
+++ packages/element-ui/vite.config.js
@@ -9,11 +9,11 @@ export default defineConfig(async ({ command, mode }) => {
   let docsBuild = {}
   // 打包组件库文档，打包 demo 组件代码
   if (mode === 'docs') {
-    docsBuild.base = sitePublic// demo 文档组件代码入口，打包成正常的单页应用，入口是 index.html。
+    docsBuild.base = `${sitePublic}element-ui/`// demo 文档组件代码入口，打包成正常的单页应用，入口是 index.html。
     docsBuild.build = { //  demo 文档示例组件构建输出目录，输出到 vitepress 目录下
       outDir: '../../docs/.vitepress/dist/element-ui',
       // 如果你的资源需要被部署到 CDN，可以设置 publicPath  
-      publicPath: `${sitePublic}/element-ui`, // 这将影响所有资源引用的路径  
+      publicDir: `${sitePublic}`, // 这将影响所有资源引用的路径  
     }
   }
   return {
