f88e0d177992c44362ed42a0e7fc26d6bd85f2fc
sewar <596777598@qq.com>
2024-03-19 10:40:12 +0800

    feat:完善发布脚本

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ fi
 # 检查deploy分支是否存在
 git branch -a | grep -qw "deploy"
 
-# 根据是否存在deploy分支，选择不同的操作
+# 根据是否存在deploy分支，创建临时分支
 if [ $? -eq 0 ]; then
     # deploy分支存在，直接切换到该分支
     git checkout deploy
@@ -33,5 +33,6 @@ git commit -m 'deploy'
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 git push -f https://github.com/Sewar-x/X-UI.git deploy:gh-pages
-
+# 删除临时分支
+git branch -D deploy
 cd -
