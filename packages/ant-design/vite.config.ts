ea81ff0f621caf6f0a4faa54cb07dedd4b60c36a
sewar <596777598@qq.com>
2024-03-27 14:13:19 +0800

    feat:添加license文件

--- packages/ant-design/vite.config.ts
+++ packages/ant-design/vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig(async ({ command, mode }) => {
       },
       lib: {
         entry: path.resolve(__dirname, './components/index.ts'),
-        name: 'xw-ui-ant',
+        name: 'XWUi',
         fileName: 'xw-ant-design',
         formats: ['es', 'cjs', 'umd', 'iife']
       }
