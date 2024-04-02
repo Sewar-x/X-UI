4ddce005c320581db3a0fec1accb1a00074bdf06
sewar <596777598@qq.com>
2024-04-02 17:39:11 +0800

    feat：调整文档文件夹名称

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -1,22 +1,22 @@
 export default [
   {
     text: '手册指南',
-    link: '/zh-CN/guide/elp-install',
+    link: '/zh-CN/guide/elu-install',
     activeMatch: '^/zh-CN/guide'
   },
   {
     text: 'Element 组件',
-    link: '/zh-CN/components/element-ui/XElementUI组件总览/XElementUI组件总览',
+    link: '/zh-CN/components/element-ui/Vue2 组件总览/Vue2 组件总览',
     activeMatch: '^/zh-CN/components/element-ui'
   },
   {
     text: 'ElementPlus 组件',
-    link: '/zh-CN/components/element-plus/XElementPlus组件总览/XElementPlus组件总览',
+    link: '/zh-CN/components/element-plus/Vue3 组件总览/Vue3 组件总览',
     activeMatch: '^/zh-CN/components/element-plus'
   },
   {
     text: 'AntDesign 组件',
-    link: '/zh-CN/components/ant-design/XAntDesign组件总览/XAntDesign组件总览',
+    link: '/zh-CN/components/ant-design/React 组件总览/React 组件总览',
     activeMatch: '^/zh-CN/components/ant-design'
   }
 ]
