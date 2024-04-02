1b31f2885a86c369e6bc4c9fae9ccef5cea1d927
sewar <596777598@qq.com>
2024-04-02 15:17:44 +0800

    feat:修改问大哥目录名称

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -6,17 +6,17 @@ export default [
   },
   {
     text: 'Element 组件',
-    link: '/zh-CN/components/element-ui/Overview 组件总览/Overview组件总览',
+    link: '/zh-CN/components/element-ui/XElementUI组件总览/XElementUI组件总览',
     activeMatch: '^/zh-CN/components/element-ui'
   },
   {
     text: 'ElementPlus 组件',
-    link: '/zh-CN/components/element-plus/Overview 组件总览/Overview组件总览',
+    link: '/zh-CN/components/element-plus/XElementPlus组件总览/XElementPlus组件总览',
     activeMatch: '^/zh-CN/components/element-plus'
   },
   {
     text: 'AntDesign 组件',
-    link: '/zh-CN/components/ant-design/Overview 组件总览/Overview组件总览',
+    link: '/zh-CN/components/ant-design/XAntDesign组件总览/XAntDesign组件总览',
     activeMatch: '^/zh-CN/components/ant-design'
   }
 ]
--- docs/zh-CN/components/element-plus/Overview 组件总览/Overview组件总览.md
+++
@@ -1,3 +0,0 @@
-# Overview 组件总览
-
-以下是 XW-UI 对 Element Plus 二次封装的所有组件。
